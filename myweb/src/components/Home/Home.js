a92e98a67f62c240a3338f9b2c6d07e43820e775
Xinyi Lu <lxyfufu@outlook.com>
2021-07-06 22:14:00 -0400

    style

--- myweb/src/components/Home/Home.js
+++ myweb/src/components/Home/Home.js
@@ -17,12 +17,45 @@ const Home = () => {
               <button>More Info</button>
             </Link>
           </div>
-          <div className="person">
+          <div className="skills">
+            <h1>Skills</h1>
+            <div className="content">
+              <div>
+                <strong>Languages:</strong>
+                <li>CSS3</li>
+                <li>HTML5</li>
+                <li>Node.js</li>
+                <li>JavaScript</li>
+              </div>
+              <div>
+                <strong>Libraries:</strong>
+                <li>Bootstrap</li>
+                <li>Express.js</li>
+                <li>Material-ui</li>
+                <li>React.js</li>
+              </div>
+              <div>
+                <strong>Tools:</strong>
+                <li>Eslint</li>
+                <li>Webpack</li>
+                <li>Chrome Dev</li>
+                <li>Git & Github</li>
+              </div>
+              <div>
+                <strong>Uint Test:</strong>
+                <li>Jest</li>
+                <li>Enzyme</li>
+                <li>React-test</li>
+                <li>SupperTest</li>
+              </div>
+            </div>
+          </div>
+          {/* <div className="person">
             <img
               src={`${process.env.PUBLIC_URL}/pic.jpeg`}
               alt="person picture"
             />
-          </div>
+          </div> */}
         </div>
       </section>
       <section>
